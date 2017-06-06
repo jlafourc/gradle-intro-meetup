ad605559e4884341677d3dade2f19e379e9c10e6
Julien Lafourcade <julien.lafourcade@gmail.com>
2017-06-06 21:31:37 +0200

    Ajout de la démo d'un projet standard Java

---
+++ demos/standard-java-project/initial/src/main/java/com/company/Greetings.java
@@ -0,0 +1,12 @@
+package com.company;
+
+
+public class Greetings {
+
+    public void greet(String name) {
+        System.out.print("Hello " + name + " !");
+    }
+
+
+
+}
---
+++ demos/standard-java-project/meetup/src/main/java/com/company/Greetings.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Greetings {
+
+    public void greet(String name) {
+        System.out.print("Hello " + StringUtils.upperCase(name) + " !");
+    }
+
+
+
+}
